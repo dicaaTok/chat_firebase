7d55f90f91cc389e8f939fec241726258f3b6d58
dicaaa <dicaageek@gmail.com>
2025-10-07 22:51:44 +0600

    принимаю и отправляю

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,13 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+    buildFeatures {
+        buildConfig = true
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+
 }
 
 dependencies {
--- app/src/main/java/com/example/chat/MainActivity.kt
+++ app/src/main/java/com/example/chat/MainActivity.kt
@@ -5,18 +5,45 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.chat.databinding.ActivityMainBinding
 import com.google.firebase.Firebase
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        val database = Firebase.database//("https://my-chat-e713d-default-rtdb.asia-southeast1.firebasedatabase.app")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val database =
+            Firebase.database//("https://my-chat-e713d-default-rtdb.asia-southeast1.firebasedatabase.app")
         val myRef = database.getReference("message")
 
-        myRef.setValue("Hello, World!")
+        binding.bsnt.setOnClickListener {
+            myRef.setValue(binding.edMessage.text.toString())
         }
+        onChangeListener(myRef)
+
     }
+
+    private fun onChangeListener(dRef: DatabaseReference) {
+        dRef.addValueEventListener(object : ValueEventListener{
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                binding.rcView.append("\n")
+                binding.rcView.append("dica: ${snapshot.value.toString()}")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        }
+        )
+    }
+}
